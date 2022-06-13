4bce93b15a615faad6326303fa0dedaf079de66d
Mathieu Acthernoene <zoontek@gmail.com>
2022-06-13 14:10:15 +0200

    Fix tests + Serializer

--- src/Serializer.ts
+++ src/Serializer.ts
@@ -11,15 +11,15 @@ export const encode = (value: any, indent?: number | undefined) => {
       if (value.__boxed_type__ === "Option") {
         return {
           __boxed_type__: "Option",
-          tag: value.value.tag,
-          value: value.value.value,
+          tag: value.tag,
+          value: value.value,
         };
       }
       if (value.__boxed_type__ === "Result") {
         return {
           __boxed_type__: "Result",
-          tag: value.value.tag,
-          value: value.value.value,
+          tag: value.tag,
+          value: value.value,
         };
       }
       if (value.__boxed_type__ === "AsyncData") {
