b9af1ee2cc524911fb79ba878a7f1c4a3e18fb91
Matthias Le Brun <bloodyowl@icloud.com>
2022-08-05 12:00:34 +0200

    Fix formating issue

--- src/Dict.ts
+++ src/Dict.ts
@@ -3,8 +3,7 @@ import { Concrete, LooseRecord } from "./types";
 export const entries = <T extends LooseRecord<unknown>>(value: T) =>
   Object.entries(value) as {
     [K in keyof Concrete<T>]: [K, T[K]];
-  }[keyof T];
-[];
+  }[keyof T][];
 
 export const keys = <T extends LooseRecord<unknown>>(value: T) =>
   Object.keys(value) as (keyof T)[];
