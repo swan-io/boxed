509b7da54158a12f8297bcc9fe94aa64de07e81c
Sacha Weatherstone <sachaw100@hotmail.com>
2024-06-09 16:04:14 +1000

    update package exports

---
+++ mod.ts
@@ -0,0 +1,9 @@
+export * as Array from "./src/Array.ts";
+export { AsyncData } from "./src/AsyncData.ts";
+export { Deferred } from "./src/Deferred.ts";
+export * as Dict from "./src/Dict.ts";
+export { Future } from "./src/Future.ts";
+export { Lazy } from "./src/Lazy.ts";
+export { Option, Result } from "./src/OptionResult.ts";
+export * as Serializer from "./src/Serializer.ts";
+export type { JsonAsyncData, JsonOption, JsonResult } from "./src/types.ts";
--- src/Boxed.ts
+++
@@ -1,9 +0,0 @@
-export * as Array from "./Array.ts";
-export { AsyncData } from "./AsyncData.ts";
-export { Deferred } from "./Deferred.ts";
-export * as Dict from "./Dict.ts";
-export { Future } from "./Future.ts";
-export { Lazy } from "./Lazy.ts";
-export { Option, Result } from "./OptionResult.ts";
-export * as Serializer from "./Serializer.ts";
-export type { JsonAsyncData, JsonOption, JsonResult } from "./types.ts";
